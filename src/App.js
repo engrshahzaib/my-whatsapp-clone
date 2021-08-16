171bdd5b2ae423ad52e960e1c6cd8346369b50d7
engrshahzaib <engr.shahzaibpp@gmail.com>
2021-08-16 00:34:07 -0700

    3rd commit

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home";
 import SignUp from "./Pages/SignUp";
 import { useState } from "react";
+import Login from "./Pages/Login";
 
 function App() {
+   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+
    const [signUp, setSignUp] = useState(false);
 
    return (
       <Container>
          <Router>
-            <SignUp setSignUp={setSignUp} />
-            <Switch>
-               <Route path='/'>{/* <Home /> */}</Route>
-            </Switch>
+            {!user && signUp ? (
+               <SignUp setSignUp={setSignUp} />
+            ) : !user && !signUp ? (
+               <Login setUser={setUser} setSignUp={setSignUp} />
+            ) : (
+               <Switch>
+                  <Route path='/'>
+                     <Home />
+                  </Route>
+               </Switch>
+            )}
          </Router>
       </Container>
    );
